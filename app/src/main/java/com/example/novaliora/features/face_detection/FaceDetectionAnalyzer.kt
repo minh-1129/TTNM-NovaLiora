39d132542cd9c0790dcb961ff6b12d271d6cf9b9
Cong <125168053+buiftheescoong@users.noreply.github.com>
2025-04-15 19:00:52 +0700

    add features: face detection, object detection

---
+++ app/src/main/java/com/example/novaliora/features/face_detection/FaceDetectionAnalyzer.kt
@@ -0,0 +1,40 @@
+package com.example.novaliora.features.face_detection
+
+import androidx.annotation.OptIn
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.example.novaliora.features.FaceDetectorProvider.faceDetector
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+
+
+class FaceDetectionAnalyzer(
+    private val onFaceDetected: (faces: MutableList<Face>, width: Int, height: Int) -> Unit
+): ImageAnalysis.Analyzer {
+
+    @OptIn(ExperimentalGetImage::class)
+    override fun analyze(image: ImageProxy) {
+        image.image?.let {
+            val imageValue = InputImage.fromMediaImage(it, image.imageInfo.rotationDegrees)
+            faceDetector.process(imageValue)
+                .addOnCompleteListener { task: Task<List<Face>> ->
+                    if (task.isSuccessful) {
+                        val faces = task.result
+                        val imageWidth = image.width
+                        val imageHeight = image.height
+
+                        // Find the most prominent face (largest bounding box)
+                        val mostProminentFace = faces.maxByOrNull { it.boundingBox.width() * it.boundingBox.height() }
+
+                        onFaceDetected(mostProminentFace?.let { mutableListOf(it) } ?: mutableListOf(), imageWidth, imageHeight)
+                    }
+                    image.image?.close()
+                    image.close()
+                }
+        }
+
+    }
+
+}

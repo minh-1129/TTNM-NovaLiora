a1b27458ac838b3d66bb1f2643bb5910fca22120
Cong <125168053+buiftheescoong@users.noreply.github.com>
2025-04-15 19:19:22 +0700

    add object detector, speech recognizer

---
+++ app/src/main/java/com/example/novaliora/ApplicationViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.novaliora
+
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.novaliora.features.FaceDetectorProvider
+import com.example.novaliora.features.face_recognition.EmbeddingStore
+import com.example.novaliora.features.face_recognition.FaceNetModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ApplicationViewModel @Inject constructor(application: Application) : AndroidViewModel(application) {
+
+    val faceNetModel: FaceNetModel by lazy {
+        FaceNetModel(application)
+    }
+
+    init {
+        // Initialize FaceNetModel in the background
+        viewModelScope.launch(Dispatchers.IO) {
+            faceNetModel
+            EmbeddingStore.initialize(application, faceNetModel)
+            FaceDetectorProvider.faceDetector
+        }
+    }
+}
